230e8156a95efd01715e62bcef307af9e0942cad
carlos <carlos.urena@student.fairfield.edu>
2019-01-08 00:21:56 -0500

    Navbar mobile fix

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -3,16 +3,27 @@ import {Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return(
-        <nav className="nav-wrapper green lighten-1">
-            <div className="container">
-                <ul className="right">
-                    <li><NavLink to="/">Check-In</NavLink></li>
-                    <li><NavLink to="/members">Members</NavLink></li>
-                    <li><NavLink to="/events">Events</NavLink></li>
-                    <li><NavLink to="/account">Account</NavLink></li>
-                </ul>
-            </div>
-        </nav>
+        <div>
+            <nav className="nav-wrapper green lighten-1">
+                <div className="container">
+                <a href="#" data-target="mobile-nav" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                    <ul className="right hide-on-med-and-down">
+                        <li><NavLink to="/">Check-In</NavLink></li>
+                        <li><NavLink to="/members">Members</NavLink></li>
+                        <li><NavLink to="/events">Events</NavLink></li>
+                        <li><NavLink to="/account">Account</NavLink></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <ul className="sidenav" id="mobile-nav">
+            <li><NavLink to="/">Check-In</NavLink></li>
+                <li><NavLink to="/members">Members</NavLink></li>
+                <li><NavLink to="/events">Events</NavLink></li>
+                <li><NavLink to="/account">Account</NavLink></li>
+            </ul>
+            
+        </div>
     )
 }
 
