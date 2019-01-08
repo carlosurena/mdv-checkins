f36c599492c7ae5e6af7864e9217042dbd60afeb
carlos <carlos.urena@student.fairfield.edu>
2019-01-07 23:52:22 -0500

    semantic ui + member changes

--- src/Components/members/Member.js
+++ src/Components/members/Member.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-
+import {NavLink, Switch, Route, BrowserRouter} from 'react-router-dom'
+import MemberStats from './memberstats'
+import { Select } from 'semantic-ui-react'
 class Member extends Component{
 
     state = {
         id: null,
-        person: null,
+        member: {"first_name": 'Carlos', "last_name": "Urena", "email":"carlosurenajr@gmail.com", "gender":"M", "phone": "2035227369", "dob" : "", "type": "Active", "createdOn": "", "updatedOn": "" },
     }
     componentDidMount(){
         let id = this.props.match.params.member_id;
@@ -14,18 +16,82 @@ class Member extends Component{
             id : id
         })
     }
+
+    handleDropdownChange = (e) => {
+        this.setState({
+            [e.target.id]: e.currentTarget.value
+        })
+    }
+
     render(){
-        const person = this.state.person ? (
-            <div className="person">PERSON LOADED!</div>
+        const currentPath = "/member/" + this.state.id + "/";
+        const membershipOptions = [
+            {key: 'Visitor', value: 'Visitor', text: 'Visitor', text_esp: 'Visita'},
+            {key: 'RegularVisitor', value: 'RegularVisitor', text: 'Regular Visitor', text_esp: 'Visita Regular'},
+            {key: 'Passive', value: 'Passive', text: 'Passive Member', text_esp: 'Miembro Pasivo'},
+            {key: 'Active', value: 'Active', text: 'Active Member', text_esp: 'Miembro Activo'} ]
+        const member = this.state.member ? (
+            <div className="">
+                <div className="section green">
+                    <div className="row valign-wrapper">
+                        <div className="col s12 m4">
+                            <h3>{this.state.member.first_name + " " + this.state.member.last_name}</h3>
+                        </div>
+                        <div className="col s12 m8">
+                            <div className="valign-wrapper">
+                               <Select placeholder='Membership Type' options={membershipOptions} value={this.state.member.type}>
+
+                               </Select>
+                                
+                            </div>
+                            
+                        </div> 
+                    </div>
+                </div>
+
+                    <div className="row event-details-content">
+                        <div className="col s12 m3 grey lighten-4 details-sidebar">
+                            <ul>
+                                <NavLink to={currentPath+"stats"}><li className="valign-wrapper">
+                                    <i className="material-icons prefix">create</i>
+                                    <span> Information</span>
+                                </li></NavLink>
+                                <NavLink to={currentPath+"sections"}><li className="valign-wrapper">
+                                    <i className="material-icons prefix">access_time</i>
+                                    <span> Check-Ins</span>
+                                </li></NavLink>
+                                <NavLink to={currentPath+"reports"}><li className="valign-wrapper">
+                                    <i className="material-icons prefix">assessment</i>
+                                    <span> Reports</span>
+                                </li></NavLink>
+                                
+                            </ul>
+                        </div>
+                        <div className="col s12 m9 grey lighten-5 details-content">
+                                <Switch>
+                                    <Route exact path={currentPath} render={(props) => <MemberStats  id={this.state.id} member={this.state.member}/>} />
+                                    <Route exact path={currentPath+"stats"} render={(props) => <MemberStats  id={this.state.id} member={this.state.member}/>} />
+                                    <Route exact path={currentPath+"sections"} render={(props) => <MemberStats  id={this.state.id} member={this.state.member}/>} />
+                                    <Route exact path={currentPath+"reports"} render={(props) => <MemberStats  id={this.state.id} member={this.state.member}/>} />
+                                    <Route exact path={currentPath+"edit"} render={(props) => <MemberStats  id={this.state.id} member={this.state.member}/>} />
+                                </Switch>
+                                
+                            
+                        </div>
+                    </div>
+                
+                
+            </div>
         ) : (
-            <div className="center">Could not find the requested person (PERSON ID {this.state.id})...</div>
+            <div className="center">Could not find the requested member (member ID {this.state.id})...</div>
             )
         return(
-            <div className="container">
-                <div className="center">
-                    {person}
+            <BrowserRouter>
+                <div>
+                    {member}
                 </div>
-            </div>
+                    
+                </BrowserRouter>
         );
     }
 }
